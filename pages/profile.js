0c82e6ef071fba077b21ae092d088bbe2e8b5e67
geunza <65527146+geunza@users.noreply.github.com>
2023-01-27 18:21:02 +0900

    zerocho next.js 시작

---
+++ pages/profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AppLayout from "../components/AppLayout";
+
+const Profile = () => {
+  const a = "AA";
+  if (a == "AA") {
+    console.log("AAAAA");
+  }
+  return (
+    <AppLayout>
+      <div>프로필 페이지</div>
+    </AppLayout>
+  );
+};
+
+export default Profile;
