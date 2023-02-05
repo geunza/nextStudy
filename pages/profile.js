6e2481837519e7f2893e2cc271a71b2574b733b5
geunyoung-sbs <safa940812@naver.com>
2023-02-06 02:41:49 +0900

    프로필 임시데이터 삽입

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,30 @@
 import React from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
-
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 const Profile = () => {
+  const followerList = [
+    { nickname: "제로초1" },
+    { nickname: "바보1" },
+    { nickname: "노드버드1" },
+  ];
+  const followingList = [
+    { nickname: "제로초2" },
+    { nickname: "바보2" },
+    { nickname: "노드버드2" },
+  ];
   return (
-    <AppLayout>
+    <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <div>프로필 페이지</div>
-    </AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followerList} />
+        <FollowList header="팔로워 목록" data={followingList} />
+      </AppLayout>
+    </>
   );
 };
 
