8fb504a6a3b1dc6675220f321962d18d0a61c4df
geunyoung-sbs <safa940812@naver.com>
2023-02-06 02:40:53 +0900

    임시로그인/3분할

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
 import "antd/dist/antd.css";
+import styled from "styled-components";
+
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -14,7 +24,7 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
-          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+          <SearchInput enterButton />
         </Menu.Item>
         <Menu.Item>
           <Link href="/profile">
@@ -27,7 +37,23 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="###" target="_blank" rel="noreferrer noopener">
+            Made by GY
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
