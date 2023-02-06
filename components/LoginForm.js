01f14f32b7e32492c1b56fdafb72b78118497979
geunyoung-sbs <safa940812@naver.com>
2023-02-07 01:22:11 +0900

    로그인 방식 리덕스로 변경

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,19 +4,22 @@ import Link from "next/link";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import useInput from "../hooks/useInput";
+import { useDispatch } from "react-redux";
+import { loginAction } from "../reducers/user";
 const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 const FormWrapper = styled(Form)`
   padding: 10px;
 `;
-const LoginForm = ({ setIsLoggedIn }) => {
+const LoginForm = ({}) => {
+  const dispatch = useDispatch();
   const [id, onChangeId] = useInput("");
   const [password, onChangePassword] = useInput("");
 
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
-    setIsLoggedIn(true);
+    dispatch(loginAction({ id, password }));
   }, [id, password]);
   const style = useMemo(() => ({ marginTop: 10 }), []);
   return (
@@ -58,7 +61,4 @@ const LoginForm = ({ setIsLoggedIn }) => {
   );
 };
 
-LoginForm.propTypes = {
-  setIsLoggedIn: PropTypes.func.isRequired,
-};
 export default LoginForm;
