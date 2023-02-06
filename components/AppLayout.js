d47a41981a201399803ee52be1212d5c7ca5b914
geunyoung-sbs <safa940812@naver.com>
2023-02-07 01:22:00 +0900

    isLoggedIn 리덕스로 변경

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
 import "antd/dist/antd.css";
 import styled from "styled-components";
-
+import { useSelector } from "react-redux";
 import UserProfile from "./UserProfile";
 import LoginForm from "./LoginForm";
 
@@ -13,7 +13,8 @@ const SearchInput = styled(Input.Search)`
 `;
 
 const AppLayout = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
   return (
     <div>
@@ -39,11 +40,7 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {isLoggedIn ? (
-            <UserProfile setIsLoggedIn={setIsLoggedIn} />
-          ) : (
-            <LoginForm setIsLoggedIn={setIsLoggedIn} />
-          )}
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
