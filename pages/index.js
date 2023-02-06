f516650cc560776e83f9c4c7a2ef01883f17e19c
geunyoung-sbs <safa940812@naver.com>
2023-02-07 01:23:27 +0900

    리덕스 isLoggedIn, mainPosts 사용, PostForm, PostCard 사용

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
+import { useSelector } from "react-redux";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
 const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
   return (
     <AppLayout>
       <Head>
         <title>NodeBird</title>
       </Head>
-      <div>Hello, Next</div>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </AppLayout>
   );
 };
