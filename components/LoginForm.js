b2076b7da925bd46726a11224eda37fb9c93d491
geunza <65527146+geunza@users.noreply.github.com>
2023-02-06 16:53:16 +0900

    커스텀훅으로 변경(useInput)

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -2,7 +2,8 @@ import React, { useCallback, useMemo, useState } from "react";
 import { Form, Input, Button } from "antd";
 import Link from "next/link";
 import styled from "styled-components";
-
+import PropTypes from "prop-types";
+import useInput from "../hooks/useInput";
 const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
@@ -10,14 +11,9 @@ const FormWrapper = styled(Form)`
   padding: 10px;
 `;
 const LoginForm = ({ setIsLoggedIn }) => {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const onChangeId = useCallback((e) => {
-    setId(e.target.value);
-  }, []);
-  const onChangePassword = useCallback((e) => {
-    setPassword(e.target.value);
-  }, []);
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
     setIsLoggedIn(true);
@@ -62,4 +58,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
   );
 };
 
+LoginForm.propTypes = {
+  setIsLoggedIn: PropTypes.func.isRequired,
+};
 export default LoginForm;
