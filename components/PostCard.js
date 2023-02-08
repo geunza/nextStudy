4c702ff47f887c4bbb700d6397eea8bc7d392af2
geunyoung-sbs <safa940812@naver.com>
2023-02-09 00:39:13 +0900

    import CommentForm

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,5 +1,5 @@
-import { Card, Popover, Button, Avatar } from "antd";
 import { useCallback, useState } from "react";
+import { Card, Popover, Button, Avatar, List, Comment } from "antd";
 import {
   EllipsisOutlined,
   HeartOutlined,
@@ -10,6 +10,7 @@ import {
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import PostImages from "./PostImages";
+import CommentForm from "./CommentForm";
 
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false);
@@ -26,7 +27,7 @@ const PostCard = ({ post }) => {
   return (
     <div style={{ marginBottom: 20 }}>
       <Card
-        cover={post.Images[0] && <PostImages image={post.Images} />}
+        cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <RetweetOutlined key="retweet" />,
           liked ? (
@@ -67,7 +68,25 @@ const PostCard = ({ post }) => {
         ></Card.Meta>
         {/* <Buttons></Buttons> */}
       </Card>
-      {commentFormOpend && <div>댓글 부분</div>}
+      {commentFormOpend && (
+        <div>
+          <CommentForm post={post} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                />
+              </li>
+            )}
+          />
+        </div>
+      )}
       {/* <CommentForm /> */}
       {/* <Comments /> */}
     </div>
